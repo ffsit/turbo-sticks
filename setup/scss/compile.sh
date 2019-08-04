34d16c3b735b8ad10d5c64bb97efde8d9c91c210
Daverball <dave@daverball.com>
2019-08-04 17:10:19 +0200

    Added responsive chat stylesheet.

--- setup/scss/compile.sh
+++ setup/scss/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sassc -t compressed style.scss style.css
 sassc -t compressed stream.scss ../../static/stream.css
+sassc -t compressed chat.scss ../../static/chat.css
 css-purge -f config.json
